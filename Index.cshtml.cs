653ad7690f1eaa75a2fdcd42462b39def61a39c9
R-star92 <82318561+R-star92@users.noreply.github.com>
2024-10-20 17:28:18 +0930

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Arcus.Security.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Hack_The_Crisis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ISecretProvider _secretProvider;
+
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private string ApiUrl => _configuration["API_URL"];
+        public string BoxId { get; set; }
+        public string ActivationCode { get; set; }
+        public string BoxStatus { get; set; }
+
+        private static int initBlobCount;
+        private static int initTxtCount;
+
+        public IndexModel(ISecretProvider secretProvider, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _secretProvider = secretProvider;
+        }
+
+        private async Task<JObject> RegisterNewBox()
+        {
+            var response = await HttpClient.PostAsync($"{ApiUrl}/api/v1/box/new", null);
+            var responseString = JObject.Parse(await response.Content.ReadAsStringAsync());
+            return responseString;
+        }
+
+        private async Task<JObject> GetBoxStatus()
+        {
+            var response =
+                await HttpClient.GetAsync($"{ApiUrl}/api/v1/box/status?boxid=" + BoxId);
+            var responseString = JObject.Parse(await response.Content.ReadAsStringAsync());
+            return responseString;
+        }
+
+        public JsonResult OnGetGetData()
+        {
+            return new JsonResult(initBlobCount + ";" + initTxtCount);
+        }
+
+        public async Task OnGet()
+        {
+            initBlobCount = -1;
+            initTxtCount = -1;
+            var apiKey = await _secretProvider.GetSecretAsync("HTC-API-Key");
+            HttpClient.DefaultRequestHeaders.Clear();
+            HttpClient.DefaultRequestHeaders.Add("x-api-key", apiKey.Value);
+
+            var autoEvent = new AutoResetEvent(false);
+            Timer _tm = new Timer(refreshData, autoEvent, 20000, 20000);
+
+            //read cookie from Request object  
+
+            string cookieBoxId = Request.Cookies["boxId"];
+            string cookieActivationCode = Request.Cookies["activationCode"];
+
+            if (Request.Query.ContainsKey("boxId"))
+                // The user is asking/forcing to connect to a different (existing!) box, so we override this
+                cookieBoxId = Request.Query["boxId"];
+
+
+            if (string.IsNullOrEmpty(cookieBoxId))
+            {
+                JObject responseString = await RegisterNewBox();
+
+                var respBoxId = responseString.GetValue("boxId").ToString();
+                var respActivationCode = responseString.GetValue("activationCode").ToString();
+                var respBoxStatus = responseString.GetValue("status").ToString();
+
+                Response.Cookies.Append("boxId", respBoxId);
+                Response.Cookies.Append("activationCode", respActivationCode);
+
+                BoxId = respBoxId;
+                ActivationCode = respActivationCode;
+                BoxStatus = respBoxStatus;
+            }
+            else
+            {
+                BoxId = cookieBoxId;
+                ActivationCode = cookieActivationCode;
+                BoxStatus = (await GetBoxStatus()).GetValue("status").ToString();
+            }
+        }
+
+        private async void refreshData(object state)
+        {
+            await GetBlobUris();
+            await GetTexts();
+        }
+
+        public async Task<List<string>> GetBlobUris()
+        {
+            List<string> blobUris = new List<string>();
+            var response =
+                await HttpClient.GetAsync($"{ApiUrl}/api/v1/content/images?boxId=" + BoxId);
+            var responseArray = JArray.Parse(await response.Content.ReadAsStringAsync());
+
+            foreach (JObject item in responseArray) blobUris.Add(item.GetValue("mediaUrl").ToString());
+
+            initBlobCount = blobUris.Count;
+
+            return blobUris;
+        }
+
+        public async Task<List<string>> GetTexts()
+        {
+            List<string> texts = new List<string>();
+            var response =
+                await HttpClient.GetAsync($"{ApiUrl}/api/v1/content/messages?boxId=" + BoxId);
+            var responseArray = JArray.Parse(await response.Content.ReadAsStringAsync());
+
+            foreach (JObject item in responseArray)
+            {
+                DateTime timeStamp = DateTime.Parse(item.GetValue("timestamp").ToString());
+                var dateTimeStr = timeStamp.ToString("HH:mm");
+                var userName = item.GetValue("userName").ToString();
+                var msgTxt = item.GetValue("text").ToString();
+                texts.Add("[ " + dateTimeStr + " ] - " + userName + " - " + msgTxt);
+            }
+
+            initTxtCount = texts.Count;
+
+            return texts;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,75 +1,23 @@
-﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Serilog;
 
-namespace CommunicationApi
+namespace Hack_The_Crisis
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger();
-
-            try
-            {
-                CreateHostBuilder(args)
-                    .Build()
-                    .Run();
-
-                return 0;
-            }
-            catch (Exception exception)
-            {
-                Log.Fatal(exception, "Host terminated unexpectedly");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            IConfiguration configuration = CreateConfiguration(args);
-            IHostBuilder webHostBuilder = CreateHostBuilder(args, configuration);
-
-            return webHostBuilder;
-        }
-
-        private static IConfiguration CreateConfiguration(string[] args)
-        {
-            IConfigurationRoot configuration =
-                new ConfigurationBuilder()
-                    .AddCommandLine(args)
-                    .AddEnvironmentVariables()
-                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                    .AddJsonFile("appsettings.dev.json", true, true)
-                    .AddJsonFile("local.settings.json", true, true)
-                    .Build();
-
-            return configuration;
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration)
-        {
-            string httpEndpointUrl = "http://+:" + configuration["ARCUS_HTTP_PORT"];
-            IHostBuilder webHostBuilder =
-                Host.CreateDefaultBuilder(args)
-                    .ConfigureAppConfiguration(configBuilder => configBuilder.AddConfiguration(configuration))
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.ConfigureKestrel(kestrelServerOptions => kestrelServerOptions.AddServerHeader = false)
-                                  .UseUrls(httpEndpointUrl)
-                                  .UseSerilog()
-                                  .UseStartup<Startup>();
-                    });
-
-            return webHostBuilder;
-        }
+        public static IWebHost BuildWebHost(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((ctx, builder) =>
+                   {
+                       builder.AddEnvironmentVariables();
+                   }
+            ).UseStartup<Startup>()
+             .Build();
     }
 }
